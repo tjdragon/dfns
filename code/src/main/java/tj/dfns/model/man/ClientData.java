9aa47fde068dcd5f2f783a47cf8f7348ef0aaa5f
NF Tree <106249836+nftree721@users.noreply.github.com>
2023-08-11 16:11:19 +0200

    v1

--- code/src/main/java/tj/dfns/model/man/ClientData.java
+++ code/src/main/java/tj/dfns/model/man/ClientData.java
@@ -3,7 +3,7 @@ package tj.dfns.model.man;
 public final class ClientData {
     public final String type = "key.get";
     public final String challenge;
-    public final String origin = "http://localhost:3000";
+    public final String origin = "https://app.dfns.ninja";
     public final boolean crossOrigin = false;
 
     public ClientData(final String challenge) {
